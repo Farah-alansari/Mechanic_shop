925420a90ac561072a72913693e9ee0051c534e1
Farah Alansari <f.n.a_85@hotmail.com>
2026-03-25 20:02:01 -0500

    fix env variable

--- config.py
+++ config.py
@@ -10,5 +10,5 @@ class TestingConfig:
     CACHE_TYPE = 'SimpleCache'
 
 class ProductionConfig:
-    SQLALCHEMY_DATABASE_URI = os.environ.get(' SQLALCHEMY_DATABASE_URI')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
     CACHE_TYPE = "SimpleCache"
